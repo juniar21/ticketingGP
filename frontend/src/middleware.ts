e7d320b4bbc6480f1139b9361352f69ae6484553
juniar21 <juniararrangbua@gmail.com>
2025-05-01 20:05:10 +0700

    ulang deploy

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,12 @@
-import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./lib/auth";
 
 export async function middleware(req: NextRequest) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    if (!token && req.nextUrl.pathname == "/"){
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-    
-    return NextResponse.next();
+  const data = await auth();
+
+  if (!data && req.nextUrl.pathname == "/") {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
 }
